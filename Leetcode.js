4acff7d79b6352ad3a7233611226995aaceb79c0
rajib <rajibislam700@gmail.com>
2022-05-09 21:20:18 +0600

    67. Add Binary

--- Leetcode.js
+++ Leetcode.js
@@ -321,3 +321,31 @@ var removeDuplicates = function (s, k) {
   return str;
 };
 console.log(removeDuplicates("abcd", 2));
+// 67. Add Binary
+var addBinary = function (a, b) {
+  let len1 = a.length;
+  let len2 = b.length;
+  let max = Math.max(len1, len2);
+  let res = "";
+  let carry = 0;
+  let val = 0;
+  for (let i = 0; i < max; i++) {
+    val =
+      Number(a[a.length - 1 - i] || 0) +
+      Number(b[b.length - 1 - i] || 0) +
+      carry;
+    carry = Math.floor(val / 2);
+    res = (val % 2) + res;
+  }
+  if (carry > 0) {
+    res = carry + res;
+  }
+  return res;
+};
+console.log(addBinary("10", "1"));
+// 69. Sqrt(x)
+var mySqrt = function (x) {
+  let res = Math.floor(Math.sqrt(x));
+  return res;
+};
+console.log(mySqrt(4));
