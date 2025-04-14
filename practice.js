c171a915ec26799a7d478a3ef5f892f884e772d8
rajib <rajibislam700@gmail.com>
2025-04-14 07:52:53 +0600

    b

--- practice.js
+++ practice.js
@@ -1,31 +1,19 @@
-var search = function (nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  console.log("Left:", left);
-  console.log("Right:", right);
-
-  while (left <= right) {
-    let mid = Math.floor(left + right) / 2;
-    console.log("MID:", mid);
-    if (nums[mid] === target) {
-      return mid;
-    } else if (nums[mid] >= nums[left]) {
-      if (nums[left] <= target && target <= nums[mid]) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
-    } else {
-      if (nums[right] >= target && target >= nums[mid]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
+var myPow = function (x, n) {
+  if (n === 0) {
+    return 1;
   }
-  return -1;
+
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+
+  if (n % 2 === 0) {
+    let half = myPow(x, n / 2);
+    return half * half;
+  } else {
+    return x * myPow(x, n - 1);
+  }
 };
 
-const nums = [4, 5, 6, 7, 0, 1, 2];
-let target = 1;
-console.log(search(nums, target));
+console.log(myPow(2, 31));
